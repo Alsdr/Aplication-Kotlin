268f5ee5d67cb9b90766f38c9c97590ddee812f4
alsdr <alessandrdm@gmail.com>
2025-06-03 21:25:52 -0400

    jhhjg

--- app/src/main/java/com/example/aplication/MainActivity.kt
+++ app/src/main/java/com/example/aplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.aplication
 
+import PessoaCard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.aplication.models.Pessoa
 import com.example.aplication.ui.theme.AplicationTheme
 import com.example.aplication.views.FichaPessoal
 
@@ -34,7 +36,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    FichaPessoal("Jonatas", 2222, listOf("Leitura","Corrida", "Cinema","Esporte", "Viajar"))
+    PessoaCard(Pessoa("Joao",17))
 }
 
 @Preview(showBackground = true)
