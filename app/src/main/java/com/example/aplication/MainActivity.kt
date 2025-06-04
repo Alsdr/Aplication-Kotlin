c99b1c1e180ed8f76bbccbbb7e36b118b13260fb
alsdr <alessandrdm@gmail.com>
2025-06-03 21:23:33 -0400

    jhhjg

---
+++ app/src/main/java/com/example/aplication/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.aplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.aplication.ui.theme.AplicationTheme
+import com.example.aplication.views.FichaPessoal
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AplicationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    FichaPessoal("Jonatas", 2222, listOf("Leitura","Corrida", "Cinema","Esporte", "Viajar"))
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AplicationTheme {
+        Greeting("Android")
+    }
+}
