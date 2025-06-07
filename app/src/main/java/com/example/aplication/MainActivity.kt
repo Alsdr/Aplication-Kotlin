518bbe0c3d57fe964edc2317b4ec861f0493366f
maxily <mmanuelly83@gmail.com>
2025-06-06 21:14:12 -0400

    m

--- app/src/main/java/com/example/aplication/MainActivity.kt
+++ app/src/main/java/com/example/aplication/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -15,7 +13,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.aplication.models.Client
@@ -25,9 +22,6 @@ import com.example.aplication.models.SaleGet
 import com.example.aplication.services.ApiService
 import com.example.aplication.ui.theme.AplicationTheme
 import com.example.aplication.views.SwipePagerScreen
-import com.example.aplication.views.exibirListaProdutoPreview
-import com.example.aplication.views.listaAPIprodutosPreview
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -65,10 +59,3 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     SwipePagerScreen(clients, products, employees, sales)
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AplicationTheme {
-        Greeting("Android")
-    }
-}
